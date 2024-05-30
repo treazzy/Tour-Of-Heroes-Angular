97bdc94d7849f75a931ea260cbf5460511ea62fb
SifundoCohort11 <sifundotreazzy@gmail.com>
2024-05-30 14:30:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,74 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+
+// import { AppComponent } from './app.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+// import { HeroesComponent } from './heroes/heroes.component';
+// import { MessagesComponent } from './messages/messages.component';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+   
+//     AppRoutingModule
+//   ],
+//   declarations: [
+//     AppComponent,
+//     DashboardComponent,
+//     HeroesComponent,
+//     HeroDetailComponent,
+    
+//     MessagesComponent,
+//           HeroSearchComponent
+//   ],
+//   bootstrap: [ AppComponent ]
+// })
+// export class AppModule { }
+
+
+
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [
-    provideClientHydration()
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    HeroSearchComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+providers : [provideHttpClient(withFetch())]
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
